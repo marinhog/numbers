4c50c202401fafd6479c7e43be311e99b200b803
Vinicius Rocha <vmrocha@gmail.com>
2016-03-08 20:44:47 -0500

    Fix pt translation

--- src/locales.js
+++ src/locales.js
@@ -14,12 +14,12 @@ var locales = {
             finish: 'Terminar',
             new: 'Novo',
             message_seconds_per_number: 'Você levou em média {0} segundos para dizer cada número.',
-            message_less_one: 'Excelente! Você está fluente nesses números em inglês!',
+            message_less_one: 'Excelente! Você está fluente nesses números em francês!',
             message_less_one_half: 'Muito bom! Continue praticando.',
             message_less_two: 'Está quase lá! Parabéns!',
             message_more_two: 'Bom! Continue praticando.'
         }
-    },
+    }
 }
 
 export default locales;
